2b30c9b4cad340dcd9f87ed80c846febf96e8e04
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-03-29 15:57:05 +0000

    use credentials rather than secrets

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  devise :omniauth_providers => [:google_oauth2]
+  devise :omniauthable, :omniauth_providers => [:google_oauth2]
 
   def self.from_omniauth(auth)
     # Either create a User record or update it based on the provider (Google) and the UID   
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -301,8 +301,8 @@ Devise.setup do |config|
 
   # Add the credentials from your Google application to your secrets
 
-  client_id = Rails.application.secrets[:google_client_id]
-  client_secret = Rails.application.secrets[:google_secret]
+  client_id = Rails.application.credentials[:google_client_id]
+  client_secret = Rails.application.credentials[:google_secret]
 
   # Configure Google omniauth with proper scope
   config.omniauth :google_oauth2, client_id, client_secret, {
