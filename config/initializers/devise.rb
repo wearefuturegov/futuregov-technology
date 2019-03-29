5f2a94bff5f83a3a1b5fc5ee4bec638b3a1522c8
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-03-29 15:41:13 +0000

    add google credentials to devise initialiser

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,6 +3,7 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
@@ -296,4 +297,16 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+
+
+  # Add the credentials from your Google application to your secrets
+
+  client_id = Rails.application.secrets[:google_client_id]
+  client_secret = Rails.application.secrets[:google_secret]
+
+  # Configure Google omniauth with proper scope
+  config.omniauth :google_oauth2, client_id, client_secret, {
+    scope: "contacts.readonly,userinfo.email"
+  }
+
 end
