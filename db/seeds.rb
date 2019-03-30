d85c23fe83bc64066562cc473a5c13004b7b56cf
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-03-30 14:20:51 +0000

    categories seedable

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+categories = [
+  "General",
+  "Computing",
+  "Networking"
+]
+
+categories.each do |name |
+  Category.create( name: name )
+end
