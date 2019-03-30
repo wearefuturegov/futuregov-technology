fb6e7430afc44d8edd7a062c987ec265b296c95b
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-03-30 15:13:27 +0000

    email saved alongside each new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     # Either create a User record or update it based on the provider (Google) and the UID   
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
       user.token = auth.credentials.token
       user.expires = auth.credentials.expires
       user.expires_at = auth.credentials.expires_at
