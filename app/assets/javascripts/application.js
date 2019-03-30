29100bf1b153bf08cc917bbf0f3d82fa64d048fe
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-03-30 22:33:56 +0000

    styled up layout and login page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,26 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+
+document.addEventListener("turbolinks:load", ()=> {
+    toggleContents()
+})
+
+
+const toggleContents = () => {
+    const toggleButton = document.querySelector(".contents__toggle")
+    const list = document.querySelector(".contents__list")
+    const toggleText = toggleButton.querySelector(".contents__toggle-title")
+    toggleButton.addEventListener("click", ()=>{
+        if(list.classList.contains("contents__list--visible")){
+            list.classList.remove("contents__list--visible")
+            toggleButton.classList.remove("contents__toggle--visible")
+            toggleText.textContent = "Show contents"
+        } else {
+            list.classList.add("contents__list--visible")
+            toggleButton.classList.add("contents__toggle--visible")
+            toggleText.textContent = "Hide contents"
+        }
+    })
+}
