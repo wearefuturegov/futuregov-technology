99a370e9cd3bc9d46ebfb4513b07e16d7e818f83
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-04-01 11:22:38 +0100

    basic crud functionality all works

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,18 +1,21 @@
 class EntriesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_categories, only: [:show, :index, :new]
+  before_action :set_categories, only: [:show, :index, :new, :edit]
+  before_action :set_entry, only: [:show, :edit, :update, :destroy]
+  before_action :set_category_options, only: [:new, :edit]
 
   def index
     @entries = Entry.all
   end
 
   def show
-    @entry = Entry.find(params[:id])
   end
 
   def new
     @entry = Entry.new
-    @categories = Category.all.map{|c| [ c.name, c.id ] }
+  end
+
+  def edit
   end
 
   def create
@@ -20,14 +23,36 @@ class EntriesController < ApplicationController
     if @entry.save
       redirect_to @entry, notice: 'Entry successfully created.'
     else
-      redirect_to @entry, notice: 'There was a problem.'
+      redirect_to @entry, notice: 'There was a problem. Please check your submission and try again.'
     end
+  end
+
+  def update
+    @entry.update(entry_params)
+    if @entry.save
+      redirect_to @entry, notice: 'Entry successfully updated.'
+    else
+      redirect_to @entry, notice: 'There was a problem. Please check your submission and try again.'
+    end
+  end
+
+  def destroy
+    @entry.destroy
+    redirect_to entries_path
   end
 
   private
 
   def set_categories
     @categories = Category.all
+  end
+
+  def set_entry
+    @entry = Entry.find(params[:id])
+  end
+
+  def set_category_options
+    @category_options = Category.all.map{|c| [ c.name, c.id ] }
   end
 
   def entry_params
