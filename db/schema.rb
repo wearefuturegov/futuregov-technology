220d19a56a71db459a8109c54b63e88f74623797
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-04-23 22:19:55 +0100

    un-borked schema/migration disagreement

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_23_103938) do
+ActiveRecord::Schema.define(version: 2019_04_23_204742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 2019_04_23_103938) do
     t.datetime "updated_at", null: false
     t.bigint "collection_id"
     t.index ["collection_id"], name: "index_categories_on_collection_id"
+  end
+
+  create_table "collections", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "entries", force: :cascade do |t|
