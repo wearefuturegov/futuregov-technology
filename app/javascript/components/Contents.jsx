2e2aa1966c30810a123d499fc6c899ff2f39e095
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-04-28 11:31:09 +0100

    scroll active entry into view

--- app/javascript/components/Contents.jsx
+++ app/javascript/components/Contents.jsx
@@ -1,23 +1,35 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
-export default ({currentCollection, currentSlug}) =>
-    <ul 
-        className="category-list" 
-        aria-live="polite" 
-        role="tabpanel" 
-        aria-labelledby={`sidebar-tab-${currentCollection.id}`}
-        >
-        {currentCollection.categories.map((category, i) => 
-            <li 
-                className="category-list__item" 
-                key={i}
-                >
-                <h2 className="category-list__title">{category.name}</h2>
-                <ul className="entry-list">
-                    {category.entries.map((entry, j)=>
-                        <li key={j}><a className={(entry.slug === currentSlug)? "entry-list__link entry-list__link--active" : "entry-list__link"} href={`/entries/${entry.slug}`}>{entry.title}</a></li>
-                    )}
-                </ul>
-            </li>    
-        )}
-    </ul>
+export default ({currentCollection, currentSlug}) => {
+
+
+    useEffect(() => {
+        // Can this be refactored into a more declarative style?
+        if(document.querySelector(".entry-list__link--active")){
+            document.querySelector(".entry-list__link--active").scrollIntoView()
+        }
+    });
+
+    return(
+        <ul 
+            className="category-list" 
+            aria-live="polite" 
+            role="tabpanel" 
+            aria-labelledby={`sidebar-tab-${currentCollection.id}`}
+            >
+            {currentCollection.categories.map((category, i) => 
+                <li 
+                    className="category-list__item" 
+                    key={i}
+                    >
+                    <h2 className="category-list__title">{category.name}</h2>
+                    <ul className="entry-list">
+                        {category.entries.map((entry, j)=>
+                            <li key={j}><a className={(entry.slug === currentSlug)? "entry-list__link entry-list__link--active" : "entry-list__link"} href={`/entries/${entry.slug}`}>{entry.title}</a></li>
+                        )}
+                    </ul>
+                </li>    
+            )}
+        </ul>
+    )
+}
